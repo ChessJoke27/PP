91bcc78cb29139dd83ef504aa963b83cf345720e
ChessJoke27 <Jesse.Koch0207@gmail.com>
2025-07-03 20:05:46 +0200

    Add navigation bar and placeholder pages

--- inventurprojekt/inventurprojekt/urls.py
+++ inventurprojekt/inventurprojekt/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='base.html'), name='home'),
+    path('', TemplateView.as_view(template_name='inventory.html'), name='home'),
+    path('auftraege/', TemplateView.as_view(template_name='orders.html'), name='orders'),
+    path('kalender/', TemplateView.as_view(template_name='calendar.html'), name='calendar'),
+    path('produkte/', TemplateView.as_view(template_name='products.html'), name='products'),
     path('admin/', admin.site.urls),
     path('api/', include('inventory.urls')),
 ]
